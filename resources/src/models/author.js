7a983e93ca37f108d20c417c382ce651bca16d99
excalibur <lzy7750015@qq.com>
2013-06-10 21:41:12 +0800

    update

--- resources/src/models/author.js
+++ resources/src/models/author.js
@@ -5,7 +5,7 @@ define(function(require, exports, module) {
   AuthorModel = Backbone.Model.extend({
     defaults: {
       name: 'excalibur',
-      img: 'resources/img/author/excalibur.jpeg',
+      img: 'https://secure.gravatar.com/avatar/a61d1f8569ef9b0e516833908b3e1afb?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
       url: 'https://github.com/excalibur'
     }
   });
